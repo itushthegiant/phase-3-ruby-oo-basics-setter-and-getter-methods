343d4d9373bb94cb601b74ed2d330b40a58fd45b
etamar <etamar.levy@gmail.com>
2021-07-26 18:43:16 -0400

    commit tree 7ba0ff42749f02c6a549fed9198c106e97955be6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name
+        @name
+    end
+
+    
 
 end
+
